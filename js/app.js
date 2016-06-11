ce04ed2a3939debc880f0aeaea9ea6d9c98e14b0
Stephanie Shipley <stephbarker74@yahoo.com>
2016-06-11 14:00:58 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+	/* add item on button click */
+	$("#btnSubmit").click(addItem);
+	/* add item when Enter key pressed when cursor is in textbox */
+	$("#txtAdd").keydown(function(event) {
+		var keycode = event.keyCode ? event.keyCode : event.which;
+		if(keycode == 13){
+			addItem();
+		}
+	});
+	/* remove item from list */
+	$("#list").on("click", "a", function() {
+		var listItem = $(this).closest("li");
+		var itemName = listItem.find("span").text();
+		var message = "Are you sure you want to remove '" + itemName + "' from the list?";
+		if(window.confirm(message)) {
+			listItem.remove();
+		}
+	});
+
+	/* strikethrough text if checkbox selected */
+	$("#list").on("change", "input:checkbox",function () {
+		var item = $(this).closest("li").find("span");
+		if($(this).is(":checked")) {
+			item.addClass("completed");
+		}
+		else
+			item.removeClass("completed"); 
+	});
+
+	/* add sorting to list */
+	$("#list").sortable({   
+		placeholder: "ui-sortable-placeholder" 
+    });  
+});
+
+function addItem() {
+	var newItem = $("#txtAdd").val();
+	if(newItem.trim().length === 0) {
+		alert("You must enter an item to be added.");
+        return;
+	}
+	var listItem = createListItem(newItem);
+	$("#list").append(listItem);
+	$("#txtAdd").val("");
+}
+function createListItem(newItem) {
+	var listItem = "<li class='ui-state-default'><input type='checkbox'>"; 
+	listItem += "<span>" + newItem + "</span>";
+	listItem += "<a href='#'>remove</a></li>";
+	return listItem; 
+}
