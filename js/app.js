a2a5005ce78d0c48271bcf4f0086315c38d64c15
stephbarker74 <stephbarker74@yahoo.com>
2016-07-07 11:40:47 -0700

    Changed Remove Button to Check Box

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
+
 $(document).ready(function() {
-	$("#btnSubmit").on('click', function(event) { 
+	$("#btnSubmit").on('click', function(event) {
 		//stop form submit
-		event.stopPropagation(); 
-		event.preventDefault(); 
+		event.stopPropagation();
+		event.preventDefault();
 		console.log("btnSubmit button clicked");
 
 		//get value of product input box
@@ -19,40 +20,35 @@ $(document).ready(function() {
 
 
 
-		//trim blank space 
-		var product = $.trim(productEntered); 
+		//trim blank space
+		var product = $.trim(productEntered);
 		//calculating length of product name
 		var productNameLength = product.length;
-		if (productNameLength === 0) { 
-			//alert if length is 0 to add item 
-			console.log("Please enter an item."); 
-			} 
-		else { 
-			console.log("valid item"); 
+		if (productNameLength === 0) {
+			//alert if length is 0 to add item
+			console.log("Please enter an item.");
+			}
+		else {
+			console.log("valid item");
 			//else if length greater than 0 add item to list
-			$("#list").prepend("<li class=''>" + 
+			$("#list").prepend("<li class=''>" +
 							"<ul class='productDetails'>" +
-			  				"<li class='quantity'>" + unitQuantityEntered + "</li> " + 
-							"<li class='product'>" + productEntered + "</li> " + 
-			  				"<li class='price'>$ " + unitPriceEntered + "</li> " + 
-			  				"<li class='remove-item'><button>Remove</button></li>" +
-			   				"</ul>" +
+			  				"<li class='quantity'>" + unitQuantityEntered + "</li> " +
+							"<li class='product'>" + productEntered + "</li> " +
+			  				"<li class='price'>$ " + unitPriceEntered + "</li> " +
+			  				"<input class='remove-item' type='checkbox'></input> Remove item" +
+								"</ul>" +
 			   				"</li>");
 
-			}; 
-			$("#txtAdd").val(" "); 
+			}
+			$("#txtAdd").val(" ");
 
-		 
+
 		});
-
-$(".remove-item").on("click", function(){
-   $(this).closest("li").remove(); 
+		$( "#remove-items" ).click(function(event) {
+		  console.log( "Handler for .click() called." );
+				event.stopPropagation();
+				event.preventDefault();
+				$('.productDetails').has('input:checked').remove();
+		});
 });
-
-//$(".remove-item").on("click", function(event) {
-	//console.log("remove item");
-    //event.preventDefault();
-    //$(this).parent().remove();
-});
-
-
